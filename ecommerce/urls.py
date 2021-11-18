7ef56ad93fd306ac274e331085c1a4dd8932bc43
mdShakilHossainNsu2018 <shakilnsu2018@gmail.com>
2021-11-18 14:26:51 +0600

    readme added

--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -72,6 +72,6 @@ urlpatterns = [
 
 
 if settings.DEBUG:
-    print(settings.STATIC_ROOT)
+    # print(settings.STATIC_ROOT)
     urlpatterns = urlpatterns + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
     urlpatterns = urlpatterns + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
